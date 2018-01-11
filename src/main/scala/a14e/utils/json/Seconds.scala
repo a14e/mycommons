c851cabf66bd1d5528e7223b2f29429587b9a00f
Andrew <m0hct3r@gmail.com>
2018-01-12 01:09:05 +0300

    compile fix

--- src/main/scala/a14e/utils/json/Seconds.scala
+++ src/main/scala/a14e/utils/json/Seconds.scala
@@ -2,7 +2,6 @@ package a14e.utils.json
 
 import java.time.Instant
 
-import shapeless.T
 
 import scala.language.implicitConversions
 
@@ -13,10 +12,10 @@ sealed trait Seconds[T] {
 
 object Seconds {
 
-  implicit def apply[T: IsTime](time: T): Seconds[T] = SecondsImpl(time)
+  implicit def apply[T: IsTime](time: T): Seconds[T] = SecondsImpl[T](time)
 
   implicit def toTime[T: IsTime](longInstant: Seconds[T]): T = {
-    longInstant.asInstanceOf[SecondsImpl].time
+    longInstant.asInstanceOf[SecondsImpl[T]].time
   }
 
   private case class SecondsImpl[T](time: T) extends Seconds[T]
@@ -32,7 +31,7 @@ object Millis {
   implicit def apply[T: IsTime](time: T): Millis[T] = MillisImpl(time)
 
   implicit def toTime[T: IsTime](longInstant: Millis[T]): T = {
-    longInstant.asInstanceOf[MillisImpl].time
+    longInstant.asInstanceOf[MillisImpl[T]].time
   }
 
   private case class MillisImpl[T](time: T) extends Millis[T]
