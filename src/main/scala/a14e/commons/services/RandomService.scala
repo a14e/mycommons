3e923cec0007302d0d50d15de3752f77960d6f36
andrew <m0hct3r@gmail.com>
2020-11-16 23:15:27 +0300

    * wrap effects into F[_]

--- src/main/scala/a14e/commons/services/RandomService.scala
+++ src/main/scala/a14e/commons/services/RandomService.scala
@@ -3,6 +3,7 @@ package a14e.commons.services
 import java.security.SecureRandom
 import java.util.UUID
 
+import cats.effect.Sync
 import com.google.common.io.BaseEncoding
 
 import scala.collection.BuildFrom
@@ -10,29 +11,32 @@ import scala.collection.generic.CanBuildFrom
 import scala.util.Random
 
 
-trait RandomService {
-  def generateUuid(): UUID
+trait RandomService[F[_]] {
+  def generateUuid(): F[UUID]
 
-  def generateHex(lenght: Int): String
+  def generateHex(lenght: Int): F[String]
 
-  def shuffle[T, C](xs: IterableOnce[T])(implicit bf: BuildFrom[xs.type, T, C]): C
+  def shuffle[T, C](xs: IterableOnce[T])(implicit bf: BuildFrom[xs.type, T, C]): F[C]
 }
 
-class RandomServiceImpl(random: Random) extends RandomService {
-  override def generateUuid(): UUID = UUID.randomUUID()
+class RandomServiceImpl[F[_] : Sync](random: Random) extends RandomService[F] {
+  protected val F = Sync[F]
 
-  override def generateHex(length: Int): String = {
+  override def generateUuid(): F[UUID] = F.delay(UUID.randomUUID())
+
+  override def generateHex(length: Int): F[String] = F.delay {
     val bytes = random.nextBytes(8)
     BaseEncoding.base16().lowerCase().encode(bytes)
   }
 
-  override def shuffle[T, C](xs: IterableOnce[T])(implicit bf: BuildFrom[xs.type, T, C]): C = {
+  override def shuffle[T, C](xs: IterableOnce[T])(implicit bf: BuildFrom[xs.type, T, C]): F[C] = F.delay {
     random.shuffle(xs)
   }
 
 }
 
 object RandomService {
-  def default(): RandomService = new RandomServiceImpl(new Random())
-  def secure(): RandomService = new RandomServiceImpl(new Random(new SecureRandom()))
+  def default[F[_] : Sync](): RandomService[F] = new RandomServiceImpl[F](new Random())
+
+  def secure[F[_] : Sync](): RandomService[F] = new RandomServiceImpl[F](new Random(new SecureRandom()))
 }
--- src/main/scala/a14e/commons/services/TimeService.scala
+++ src/main/scala/a14e/commons/services/TimeService.scala
@@ -2,14 +2,17 @@ package a14e.commons.services
 
 import java.time.{Clock, Instant}
 
+import cats.effect.Sync
 
-trait TimeService {
-  def now: Instant
-  def clock: Clock
+
+trait TimeService[F[_]] {
+  def now: F[Instant]
+  def clock: F[Clock]
 }
 
-final class TimeServiceImpl extends TimeService {
-  override def now: Instant = Instant.now()
-  override def clock: Clock = Clock.systemUTC()
+final class TimeServiceImpl[F[_]: Sync] extends TimeService[F] {
+  protected val F = Sync[F]
+  override def now: F[Instant] = F.delay(Instant.now())
+  override def clock: F[Clock] = F.delay(Clock.systemUTC())
 }
 
