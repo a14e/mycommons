51ebd5d1093344afa46afc498c2e2660f891d0f8
andrew <m0hct3r@gmail.com>
2020-12-24 00:59:17 +0300

    * upversion * revert of mdc passing

--- src/main/scala/a14e/commons/catseffect/CatsIoImplicits.scala
+++ src/main/scala/a14e/commons/catseffect/CatsIoImplicits.scala
@@ -13,8 +13,6 @@ object CatsIoImplicits {
 
     private implicit def implicitF = sync
 
-    def apply[B](block: => B): F[B] = Sync[F].delay(block)
-
     def serially[A, B](col: Seq[A])(f: A => F[B]): F[List[B]] = {
       col.foldLeft(Sync[F].pure(List.empty[B])) { (colF, x) =>
         for {
@@ -26,14 +24,5 @@ object CatsIoImplicits {
   }
 
 
-  implicit class RichEffectRunnable[F[_], A](val io: F[A]) extends AnyVal {
-
-    import cats.implicits._
-    import cats.effect.implicits._
-    def unsafeRunSync()(implicit effect: Effect[F]): A = io.toIO.unsafeRunSync()
-    def unsafeToFuture()(implicit effect: Effect[F]): Future[A] = io.toIO.unsafeToFuture()
-    def unsafeRunAsyncAndForget()(implicit effect: Effect[F]): Unit = io.toIO.unsafeRunAsyncAndForget()
-
-  }
 
 }
